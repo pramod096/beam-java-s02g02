83a7f8b7e86411a9c074bd60cd89bedc3f3372c4
vivekd31 <drakshapallyvivek31@gmail.com>
2022-04-19 21:23:50 -0500

    created all the files with necassary requirements

---
+++ VivekDrakshapally/src/main/java/edu/nwmissouri/s2g2/drakshapally/JobOnePageRankDrakshapally.java
@@ -0,0 +1,89 @@
+package edu.nwmissouri.s2g2.drakshapally;
+
+import java.util.Arrays;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.FlatMapElements;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionList;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.values.TypeDescriptors;
+
+public class JobOnePageRankDrakshapally {
+
+// static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+//     @ProcessElement
+//     public void processElement(@Element KV<String, Iterable<String>> element,
+//         OutputReceiver<KV<String, RankedPage>> receiver) {
+//       Integer contributorVotes = 0;
+//       if (element.getValue() instanceof Collection) {
+//         contributorVotes = ((Collection<String>) element.getValue()).size();
+//       }
+//       ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+//       for (String voterName : element.getValue()) {
+//         if (!voterName.isEmpty()) {
+//           voters.add(new VotingPage(voterName, contributorVotes));
+//         }
+//       }
+//       receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+//     }
+//   }
+  public static void main(String[] args) {
+
+    PipelineOptions options = PipelineOptionsFactory.create();
+    Pipeline p = Pipeline.create(options);
+    String dataFolder = "Sportsweb";
+    
+    PCollection<KV<String, String>> pcol1 = DrakshapallyMapper(p, dataFolder, "Cricket.md");
+
+    PCollection<KV<String, String>> pcol2 = DrakshapallyMapper(p, dataFolder, "Football.md");
+
+    
+    PCollection<KV<String, String>> pcol3 = DrakshapallyMapper(p, dataFolder, "Hockey.md");
+
+   
+    PCollection<KV<String, String>> pcol4 = DrakshapallyMapper(p, dataFolder, "Basketball.md");
+
+       PCollection<KV<String, String>> pcol5 = DrakshapallyMapper(p, dataFolder, "Sports.md");
+
+
+    PCollectionList<KV<String, String>> pcolSportsList = PCollectionList.of(pcol1).and(pcol2).and(pcol3).and(pcol4).and(pcol5);
+
+    PCollection<KV<String, String>> mergedList = pcolSportsList.apply(Flatten.<KV<String, String>>pCollections());
+    PCollection<KV<String, Iterable<String>>> urlToDocs = mergedList.apply(GroupByKey.<String, String>create());
+
+    PCollection<String> pLinksStr = urlToDocs.apply(
+        MapElements.into(
+            TypeDescriptors.strings())
+            .via((mergeOut) -> mergeOut.toString()));
+
+    pLinksStr.apply(TextIO.write().to("drakshapallyhoboneoutput"));
+
+    p.run().waitUntilFinish();
+  }
+
+  private static PCollection<KV<String, String>> DrakshapallyMapper(Pipeline p, String dataFolder, String dataFile) {
+    String dataLocation = dataFolder + "/" + dataFile;
+    PCollection<String> pcolInputLines = p.apply(TextIO.read().from(dataLocation));
+
+    PCollection<String> pcolLinkLines = pcolInputLines.apply(Filter.by((String line) -> line.startsWith("[")));
+    PCollection<String> pcolLinkPages = pcolLinkLines.apply(MapElements.into(TypeDescriptors.strings())
+        .via(
+            (String linkline) -> linkline.substring(linkline.indexOf("(") + 1, linkline.length() - 1)));
+    PCollection<KV<String, String>> pcolKVpairs = pcolLinkPages.apply(MapElements
+        .into(
+            TypeDescriptors.kvs(
+                TypeDescriptors.strings(), TypeDescriptors.strings()))
+        .via(outlink -> KV.of(dataFile, outlink)));
+    return pcolKVpairs;
+
+  }
+}
