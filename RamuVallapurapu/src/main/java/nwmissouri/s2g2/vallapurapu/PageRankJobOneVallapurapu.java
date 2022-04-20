6e0dd53b6deb5710091c9a6cf952409861549433
s541910 <s541910@nwmissouri.edu>
2022-04-19 22:12:17 -0500

    added jobers

--- RamuVallapurapu/src/main/java/nwmissouri/s2g2/vallapurapu/PageRankJobOneVallapurapu.java
+++ RamuVallapurapu/src/main/java/nwmissouri/s2g2/vallapurapu/PageRankJobOneVallapurapu.java
@@ -15,6 +15,29 @@ import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class PageRankJobOneVallapurapu {
 
+	 static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<String>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Integer contributorVotes = 0;
+      if (element.getValue() instanceof Collection) {
+        contributorVotes = ((Collection<String>) element.getValue()).size();
+      }
+      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+      for (String voterName : element.getValue()) {
+        if (!voterName.isEmpty()) {
+          voters.add(new VotingPage(voterName, contributorVotes));
+        }
+      }
+      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+    }
+  }
+
+  static class Job2Mapper extends DoFn<KV<String, RankedPage>, KV<String, RankedPage>> {}
+
+  
+  static class Job2Updater extends DoFn<KV<String, Iterable<RankedPage>>, KV<String, RankedPage>> {}
+
 	public static void main(String[] args) {
 
 		PipelineOptions options = PipelineOptionsFactory.create();
