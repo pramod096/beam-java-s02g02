607884d908e337d8f010eba8a8f839f9d5e29c75
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-04-24 23:38:41 -0500

    updated the Custom java file with Job1Finalizer DocFn Class

--- SaikiranReddyGangidi/src/main/java/edu/nwmissouri/s2g2/gangidi/JobOnePageRankGangidi.java
+++ SaikiranReddyGangidi/src/main/java/edu/nwmissouri/s2g2/gangidi/JobOnePageRankGangidi.java
@@ -1,16 +1,21 @@
 package edu.nwmissouri.s2g2.gangidi;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
 import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
@@ -18,6 +23,37 @@ import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class JobOnePageRankGangidi {
+
+  // DEFINE DOFNS
+  // ==================================================================
+  // You can make your pipeline assembly code less verbose by defining
+  // your DoFns statically out-of-line.
+  // Each DoFn<InputT, OutputT> takes previous output
+  // as input of type InputT
+  // and transforms it to OutputT.
+  // We pass this DoFn to a ParDo in our pipeline.
+
+  
+
+  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<String>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Integer contributorVotes = 0;
+      if (element.getValue() instanceof Collection) {
+        contributorVotes = ((Collection<String>) element.getValue()).size();
+      }
+      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+      for (String voterName : element.getValue()) {
+        if (!voterName.isEmpty()) {
+          voters.add(new VotingPage(voterName, contributorVotes));
+        }
+      }
+      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+    }
+  }
+
+  
 
   public static void main(String[] args) {
 
@@ -46,14 +82,23 @@ public class JobOnePageRankGangidi {
         .and(pcol5).and(pcol6);
 
     PCollection<KV<String, String>> mergedList = pcolCricketList.apply(Flatten.<KV<String, String>>pCollections());
-    PCollection<KV<String, Iterable<String>>> urlToDocs = mergedList.apply(GroupByKey.<String, String>create());
+    PCollection<KV<String, Iterable<String>>> urlToStringList = mergedList.apply(GroupByKey.<String, String>create());
 
-    PCollection<String> pLinksStr = urlToDocs.apply(
-        MapElements.into(
-            TypeDescriptors.strings())
-            .via((mergeOut) -> mergeOut.toString()));
+    PCollection<KV<String, RankedPage>> job2in = urlToStringList.apply(ParDo.of(new Job1Finalizer()));
 
-    pLinksStr.apply(TextIO.write().to("gangidijoboneout"));
+    PCollection<KV<String, RankedPage>> job2out = null;
+
+    // int iterations = 2;
+    // for (int i = 1; i <= iterations; i++) {
+    //   job2out = runJob2Iteration(job2in);
+    //   job2in = job2out;
+
+    // }
+    PCollection<String> outputjob2comp = job2out.apply(MapElements.into(
+        TypeDescriptors.strings())
+        .via(kv -> kv.toString()));
+
+    outputjob2comp.apply(TextIO.write().to("GangidiJobtwooutput"));
 
     p.run().waitUntilFinish();
   }
