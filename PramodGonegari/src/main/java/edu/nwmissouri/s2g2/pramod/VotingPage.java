ffebedb0e9f91acda402877788b53d0eb64690ca
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2022-04-25 21:25:52 -0500

    added constructors and getters/setters

--- PramodGonegari/src/main/java/edu/nwmissouri/s2g2/pramod/VotingPage.java
+++ PramodGonegari/src/main/java/edu/nwmissouri/s2g2/pramod/VotingPage.java
@@ -2,9 +2,52 @@ package edu.nwmissouri.s2g2.pramod;
 
 import java.io.Serializable;
 
-public class VotingPage implements Serializable {
-       
-    public VotingPage(String s, Integer i) {
+import org.apache.beam.sdk.options.Default.Integer;
 
+class VotingPage implements Serializable {
+    private String name;
+    private Double rank;
+    private Integer votes;
+
+    public VotingPage(String name, Integer votes) {
+        this.name = name;
+        this.votes = votes;
+
+    }
+
+    public VotingPage(String name, Double rank, Integer votes) {
+        this.name = name;
+        this.rank = rank;
+        this.votes = votes;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getRank() {
+        return rank;
+    }
+
+    public void setRank(Double rank) {
+        this.rank = rank;
+    }
+
+    public Integer getVotes() {
+        return votes;
+    }
+
+    public void setVotes(Integer votes) {
+        this.votes = votes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.6f %s", name, rank, votes);
     }
 }
