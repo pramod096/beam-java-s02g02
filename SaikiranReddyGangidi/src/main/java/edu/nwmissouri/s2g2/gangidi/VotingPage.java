38bea2483ad68c69e394cec5354f309914d0d2eb
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2022-04-26 11:26:27 -0500

    updated the Voting Page with required constructor and methods

--- SaikiranReddyGangidi/src/main/java/edu/nwmissouri/s2g2/gangidi/VotingPage.java
+++ SaikiranReddyGangidi/src/main/java/edu/nwmissouri/s2g2/gangidi/VotingPage.java
@@ -1,6 +1,52 @@
 package edu.nwmissouri.s2g2.gangidi;
 
-class VotingPage {
+import java.io.Serializable;
+
+class VotingPage implements Serializable {
+
+    String votername = "unknown.md";
+    Integer contributorVotes;
+    Double rank = 1.00;
+
     public VotingPage(String voterName, Integer contributorVotes) {
+        this.votername = voterName;
+        this.contributorVotes = contributorVotes;
     }
+
+    public VotingPage(String votername, Double rank, Integer contributorVotes) {
+        this.votername = votername;
+        this.rank = rank;
+        this.contributorVotes = contributorVotes;
+
+    }
+
+    public String getVotername() {
+        return votername;
+    }
+
+    public void setVotername(String votername) {
+        this.votername = votername;
+    }
+
+    public Integer getContributorVotes() {
+        return contributorVotes;
+    }
+
+    public void setContributorVotes(Integer contributorVotes) {
+        this.contributorVotes = contributorVotes;
+    }
+
+    public Double getRank() {
+        return rank;
+    }
+
+    public void setRank(Double rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "VotingPage [contributorVotes=" + contributorVotes + ", rank=" + rank + ", votername=" + votername + "]";
+    }
+
 }
